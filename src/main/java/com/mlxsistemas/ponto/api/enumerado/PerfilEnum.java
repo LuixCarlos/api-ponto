450e43f2cbaf6f24cb4fb01a9cd668cf2c610585
Luiz <luiz.carlos.limajf@gmail.com>
2018-05-31 10:48:42 -0300

    Criação da Entidades

---
+++ src/main/java/com/mlxsistemas/ponto/api/enumerado/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.mlxsistemas.ponto.api.enumerado;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/java/com/mlxsistemas/ponto/api/enumerado/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.mlxsistemas.ponto.api.enumerado;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_INTERVALO,
+	TERMINO_INTERVALO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA;
+}
